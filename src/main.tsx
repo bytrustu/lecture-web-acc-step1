d4ccdd9ce98a6735ed7b688367cedf40e4e7e183
bytrustu <bytrustu@gmail.com>
2024-04-01 02:04:59 +0900

    refactor: main eslint issue 개선

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
+import { Layer } from '@/components/Layer.tsx';
 import Router from '@/Routes';
 import '@/styles/index.css';
 import 'react-toastify/dist/ReactToastify.css';
-import { Layer } from '@/components/Layer.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
